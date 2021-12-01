6643ec6e63cbaf4c03ed13c4ef414b789e17d1f2
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2021-12-01 08:10:56 +0100

    Add day 1

---
+++ days/day01.py
@@ -0,0 +1,19 @@
+from lib import load_input
+
+
+def solve(data):
+    # return part_one(list(map(int, list(data.splitlines()))))
+    return part_two(list(map(int, list(data.splitlines()))))
+
+
+def part_one(data):
+    return sum(data[i] > data[i - 1] for i in range(1, len(data)))
+
+
+def part_two(data):
+    return sum(data[i] < data[i+3] for i in range(len(data) - 3))
+
+
+if __name__ == '__main__':
+    print(solve(load_input('small')))
+    print(solve(load_input()))

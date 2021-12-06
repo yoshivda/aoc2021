c1c42375df4c2d77fe7672deee37cbd715c071d7
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2021-12-06 12:55:48 +0100

    Add day 6

---
+++ days/day06.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+from lib import load_input
+
+
+def solve(data):
+    # return part_one(list(map(int, data.split(','))))
+    return part_two(list(map(int, data.split(','))))
+
+
+def part_one(fish):
+    for _ in range(80):
+        for i in range(len(fish)):
+            if fish[i] == 0:
+                fish[i] = 6
+                fish.append(8)
+            else:
+                fish[i] -= 1
+    return len(fish)
+
+
+def part_two(data):
+    increases = defaultdict(int)
+    fish = len(data)
+    for f in data:
+        increases[f] += 1
+
+    for day in range(256):
+        fish += increases[day]
+        increases[day + 9] += increases[day]
+        increases[day + 7] += increases[day]
+    return fish
+
+
+if __name__ == '__main__':
+    print(solve(load_input('small')))
+    print(solve(load_input()))

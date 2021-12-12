dad001656642554ff556ad2a6cc78ac84ca96fa4
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2021-12-12 14:12:51 +0100

    Optimise day 12

--- days/day12.py
+++ days/day12.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import cache
 
 from lib import load_input
 
@@ -10,22 +11,27 @@ def solve(data):
         graph[f].append(t)
         graph[t].append(f)
 
-    # return part_one(graph, 'start', [])
-    return part_two(graph, 'start', [])
+    # return part_one(graph, 'start', set())
+    return part_two(graph, 'start')
 
 
-def part_one(graph, node, path):
+def part_one(graph, node, seen):
     if node == 'end':
         return 1
-    return sum(part_one(graph, nb, path + [node]) for nb in graph[node] if nb.isupper() or nb not in path)
+    return sum(part_one(graph, nb, seen | {node}) for nb in graph[node] if nb.isupper() or nb not in seen)
 
 
-def part_two(graph, node, path):
-    if node == 'end':
-        return 1
-    cur_path = path + [node]
-    return sum(part_two(graph, nb, cur_path) for nb in graph[node] if nb.isupper() or nb not in path or
-               (all(cur_path.count(n) == 1 for n in cur_path if n.islower()) and nb not in ('start', 'end')))
+def part_two(graph, node):
+    @cache
+    def rec_helper(node, seen, duplicate):
+        if node == 'end':
+            return 1
+        new_seen = frozenset(seen | {node})
+        res = sum(rec_helper(nb, new_seen, duplicate) for nb in graph[node] if nb.isupper() or nb not in seen)
+        if not duplicate:
+            res += sum(rec_helper(nb, new_seen, True) for nb in graph[node] if nb.islower() and nb not in ('start', 'end') and nb in seen)
+        return res
+    return rec_helper(node, frozenset(), False)
 
 
 if __name__ == '__main__':
